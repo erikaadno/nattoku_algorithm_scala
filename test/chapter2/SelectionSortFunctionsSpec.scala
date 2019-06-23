ee09212ab52ee6a9b05635e505f9ba25079a98e2
erikaadno <erika.ando+github@bizreach.co.jp>
2019-06-23 12:22:48 +0900

    refactoring test

--- test/chapter2/SelectionSortFunctionsSpec.scala
+++ test/chapter2/SelectionSortFunctionsSpec.scala
@@ -1,5 +1,6 @@
 package chapter2
 
+import org.scalatest.prop.TableDrivenPropertyChecks._
 import org.scalatest.{FunSpec, Matchers}
 
 class SelectionSortFunctionsSpec extends FunSpec with Matchers {
@@ -7,32 +8,16 @@ class SelectionSortFunctionsSpec extends FunSpec with Matchers {
   val selectionSortFunctions = new SelectionSortFunctions()
 
   describe("selection sort test") {
-    it("elements: 3") {
-      val expected = Seq(1, 2, 3)
-      val target = Seq(3, 2, 1)
-      val actual = selectionSortFunctions.selectionSort(target)
-      actual should be(expected)
-    }
 
-    it("elements: 5") {
-      val expected = Seq(1, 2, 3, 4, 5)
-      val target = Seq(3, 2, 1, 5, 4)
-      val actual = selectionSortFunctions.selectionSort(target)
-      actual should be(expected)
-    }
-
-    it("elements: many") {
-      val expected = Seq(1, 2, 3, 4, 5, 20, 30, 55)
-      val target = Seq(3, 2, 1, 5, 4, 55, 30, 20)
-      val actual = selectionSortFunctions.selectionSort(target)
-      actual should be(expected)
-    }
-
-    it("elements: minus") {
-      val expected = Seq(-2, 1, 2, 3, 4, 5, 20, 30, 55)
-      val target = Seq(3, 2, 1, 5, 4, 55, 30, 20, -2)
-      val actual = selectionSortFunctions.selectionSort(target)
-      actual should be(expected)
+    val testCases = Table(
+      ("target", "expected"),
+      (Seq(3, 2, 1), Seq(1, 2, 3)),
+      (Seq(3, 2, 1, 5, 4), Seq(1, 2, 3, 4, 5)),
+      (Seq(3, 2, 1, 5, 4, 55, 30, 20), Seq(1, 2, 3, 4, 5, 20, 30, 55)),
+      (Seq(3, 2, 1, 5, 4, 55, 30, 20, -2),  Seq(-2, 1, 2, 3, 4, 5, 20, 30, 55))
+    )
+    forAll(testCases) { (target: Seq[Int], expected: Seq[Int]) =>
+      selectionSortFunctions.selectionSort(target) should be(expected)
     }
   }
 }
